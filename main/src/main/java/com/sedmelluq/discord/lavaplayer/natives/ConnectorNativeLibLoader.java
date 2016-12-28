cce3f1e51d829b44344c6f2c9889734820f43483
sedmelluq <sedmelluq@gmail.com>
2016-12-28 16:00:05 +0200

    Windows native lib loading fix.

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/natives/ConnectorNativeLibLoader.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/natives/ConnectorNativeLibLoader.java
@@ -11,9 +11,9 @@ public class ConnectorNativeLibLoader {
    * Loads the connector library with its dependencies for the current system
    */
   public static void loadConnectorLibrary() {
-    NativeLibLoader.load(ConnectorNativeLibLoader.class, WIN_X86_64, "libmpg123-0");
-    NativeLibLoader.load(ConnectorNativeLibLoader.class, WIN_X86, "libgcc_s_sjlj-1");
-    NativeLibLoader.load(ConnectorNativeLibLoader.class, WIN_X86, "libmpg123-0");
+    NativeLibLoader.load(ConnectorNativeLibLoader.class, "libmpg123-0", WIN_X86_64);
+    NativeLibLoader.load(ConnectorNativeLibLoader.class, "libgcc_s_sjlj-1", WIN_X86);
+    NativeLibLoader.load(ConnectorNativeLibLoader.class, "libmpg123-0", WIN_X86);
     NativeLibLoader.load(ConnectorNativeLibLoader.class, "connector");
   }
 }
