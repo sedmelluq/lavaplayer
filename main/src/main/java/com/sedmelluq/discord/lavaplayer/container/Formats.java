c2906918f55731b729cfb2f42436861cb899ef22
sedmelluq <sedmelluq@gmail.com>
2017-04-10 21:38:58 +0300

    Support video+aac format for youtube videos.

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/container/Formats.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/container/Formats.java
@@ -7,6 +7,7 @@ public class Formats {
   public static final String MIME_AUDIO_WEBM = "audio/webm";
   public static final String MIME_VIDEO_WEBM = "video/webm";
   public static final String MIME_AUDIO_MP4 = "audio/mp4";
+  public static final String MIME_VIDEO_MP4 = "video/mp4";
 
   public static final String CODEC_OPUS = "opus";
   public static final String CODEC_VORBIS = "vorbis";
