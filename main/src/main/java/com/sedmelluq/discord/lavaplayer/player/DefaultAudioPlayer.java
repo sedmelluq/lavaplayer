0636cd39021b55acb9324bb938d613cab0a4f49a
Devoxin <lukel9914@gmail.com>
2018-06-04 23:35:44 +0100

    Remove upper volume limit from DefaultAudioPlayer

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/player/DefaultAudioPlayer.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/player/DefaultAudioPlayer.java
@@ -274,7 +274,7 @@ public class DefaultAudioPlayer implements AudioPlayer, TrackStateListener {
   }
 
   public void setVolume(int volume) {
-    options.volumeLevel.set(Math.min(150, Math.max(0, volume)));
+    options.volumeLevel.set(Math.max(0, volume));
   }
 
   public void setFilterFactory(PcmFilterFactory factory) {
