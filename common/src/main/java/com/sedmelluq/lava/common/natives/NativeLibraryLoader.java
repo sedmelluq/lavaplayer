0f18394d3928e202a408078a85768d28d403cebb
sedmelluq <sedmelluq@gmail.com>
2019-02-22 17:07:33 +0200

    Moved a class to correct package.

--- common/src/main/java/com/sedmelluq/lava/common/natives/NativeLibraryLoader.java
+++ common/src/main/java/com/sedmelluq/lava/common/natives/NativeLibraryLoader.java
@@ -1,6 +1,5 @@
 package com.sedmelluq.lava.common.natives;
 
-import com.sedmelluq.lava.common.natives.architecture.ResourceNativeLibraryBinaryProvider;
 import com.sedmelluq.lava.common.natives.architecture.SystemType;
 import java.io.FileOutputStream;
 import java.io.IOException;
--- common/src/main/java/com/sedmelluq/lava/common/natives/ResourceNativeLibraryBinaryProvider.java
+++ common/src/main/java/com/sedmelluq/lava/common/natives/ResourceNativeLibraryBinaryProvider.java
@@ -1,6 +1,6 @@
-package com.sedmelluq.lava.common.natives.architecture;
+package com.sedmelluq.lava.common.natives;
 
-import com.sedmelluq.lava.common.natives.NativeLibraryBinaryProvider;
+import com.sedmelluq.lava.common.natives.architecture.SystemType;
 import java.io.InputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
