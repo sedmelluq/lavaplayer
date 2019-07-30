d1279160e6a4750f1aa7d8c6bcc1d35acbae063f
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-07-30 03:18:53 +0200

    Patch moved youtube properties (#199)
    * Patch moved youtube properties
    
    * Use old format if player_response is missing

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeAudioSourceManager.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeAudioSourceManager.java
@@ -209,10 +209,19 @@ public class YoutubeAudioSourceManager implements AudioSourceManager, HttpConfig
         throw new FriendlyException(args.get("reason").text(), COMMON, null);
       }
 
-      boolean isStream = "1".equals(args.get("live_playback").text());
-      long duration = isStream ? Long.MAX_VALUE : args.get("length_seconds").as(Long.class) * 1000;
+      boolean useOldFormat = args.get("player_response").isNull();
 
-      return buildTrackObject(videoId, args.get("title").text(), args.get("author").text(), isStream, duration);
+      if (useOldFormat) {
+        boolean isStream = "1".equals(args.get("live_playback").text());
+        long duration = isStream ? Long.MAX_VALUE : args.get("length_seconds").as(Long.class) * 1000;
+        return buildTrackObject(videoId, args.get("title").text(), args.get("author").text(), isStream, duration);
+      }
+
+      JsonBrowser videoDetails = JsonBrowser.parse(args.get("player_response").text()).get("videoDetails");
+      boolean isStream = videoDetails.get("isLiveContent").as(Boolean.class);
+      long duration = isStream ? Long.MAX_VALUE : videoDetails.get("lengthSeconds").as(Long.class) * 1000;
+
+      return buildTrackObject(videoId, videoDetails.get("title").text(), videoDetails.get("author").text(), isStream, duration);
     } catch (Exception e) {
       throw ExceptionTools.wrapUnfriendlyExceptions("Loading information for a YouTube track failed.", FAULT, e);
     }
