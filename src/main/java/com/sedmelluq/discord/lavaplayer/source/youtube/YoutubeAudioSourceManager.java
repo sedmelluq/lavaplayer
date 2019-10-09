e9fd51f66ac9774c2debb9b24e33560a749bbb65
Devoxin <15076404+Devoxin@users.noreply.github.com>
2019-10-09 20:15:35 +0100

    Update YoutubeAudioSourceManager.java

--- src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeAudioSourceManager.java
+++ src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeAudioSourceManager.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
