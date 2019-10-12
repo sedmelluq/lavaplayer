c7fbe4e019cb34c60e986e828d747eb1d4db4137
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-10-12 13:28:34 +0200

    Add method to generate random /64 subnet

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/tools/Ipv6Subnet.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/tools/Ipv6Subnet.java
@@ -3,6 +3,7 @@ package com.sedmelluq.discord.lavaplayer.tools;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,33 +12,61 @@ import java.util.regex.Pattern;
  */
 public class Ipv6Subnet {
 
-  private final Pattern cidrRegex = Pattern.compile("([\\da-f:]+)/(\\d{1,3})");
+  private static final int IPV6_BIT_SIZE = 128;
+  private static final int TRUNCATED_BITS = 64;
+  private static final Random random = new Random();
+
+  private static final Pattern CIDR_REGEX = Pattern.compile("([\\da-f:]+)/(\\d{1,3})");
   private final String cidr;
   private final int maskBits; // 1-128
   private final long prefix;
 
   public Ipv6Subnet(String cidr) {
     this.cidr = cidr.toLowerCase();
-    Matcher matcher = cidrRegex.matcher(this.cidr);
+    Matcher matcher = CIDR_REGEX.matcher(this.cidr);
     if (!matcher.find()) {
       throw new IllegalArgumentException(cidr + " does not appear to be a valid CIDR.");
     }
 
+    long unboundedPrefix;
     try {
-      InetAddress byName = InetAddress.getByName(matcher.group(1));
-      prefix = addressToLong((Inet6Address) byName);
+      InetAddress address = InetAddress.getByName(matcher.group(1));
+      unboundedPrefix = addressToLong((Inet6Address) address);
     } catch (UnknownHostException e) {
       throw new IllegalArgumentException("Invalid IPv6 address", e);
     }
     maskBits = Integer.parseInt(matcher.group(2));
 
-    if (maskBits > 64 || maskBits < 1) {
-      throw new IllegalArgumentException("This class only handles with /1-64 subnets. Got /" + maskBits);
+    if (maskBits > TRUNCATED_BITS || maskBits < 1) {
+      throw new IllegalArgumentException("This class only handles /1-64 subnets. Got /" + maskBits);
     }
+
+    // Truncate all bits after $maskBits$ number of bits in the prefix
+    @SuppressWarnings("ShiftOutOfRange")
+    long prefixMask = Long.MAX_VALUE << (IPV6_BIT_SIZE - maskBits);
+    prefix = unboundedPrefix & prefixMask;
+  }
+
+  /**
+   * @return a random /64 member subnet of this subnet.
+   */
+  public InetAddress getRandomSlash64() {
+    // Create a mask of variable length to be AND'ed with a random value
+    int prefixBits = IPV6_BIT_SIZE - maskBits;
+    long randMask = Long.MAX_VALUE >> prefixBits;
+    long maskedRandom = random.nextLong() & randMask;
+
+    // Combine prefix and match
+    return longToAddress(prefix | maskedRandom);
+  }
+
+  @Override
+  public String toString() {
+    return cidr;
   }
 
   private static InetAddress longToAddress(long l) {
-    byte[] b = new byte[] {
+    byte[] b = new byte[]{
         (byte) l,
         (byte) (l >> 8),
         (byte) (l >> 16),
@@ -53,7 +82,7 @@ public class Ipv6Subnet {
     try {
       return Inet6Address.getByAddress(b);
     } catch (UnknownHostException e) {
-      throw new RuntimeException(e);
+      throw new RuntimeException(e); // This should not happen, as we do not do a DNS lookup
     }
   }
 
