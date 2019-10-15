c769f5445d6e478e9a9228eedfa3458fcfa87565
Devoxin <lukel9914@gmail.com>
2019-10-15 18:38:14 +0100

    CaNnOt FiNd SyMbOl

--- src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/CacheProvider.java
+++ src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/CacheProvider.java
@@ -1,7 +1,5 @@
 package com.sedmelluq.discord.lavaplayer.source.youtube;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public interface CacheProvider {
 
@@ -11,32 +9,30 @@ public interface CacheProvider {
      * @param format The track format, with a deciphered URL.
      * @param ttl The amount of time the URL lasts, in milliseconds.
      */
-    void cacheVideoFormat(@Nonnull String identifier, @Nonnull YoutubeAudioTrack.FormatWithUrl format, long ttl);
+    void cacheVideoFormat(String identifier, YoutubeAudioTrack.FormatWithUrl format, long ttl);
 
     /**
      * Caches the unavailability reason for the given identifier.
      * @param identifier The video ID.
      * @param unavailableReason The reason why the video is unavailable.
      */
-    void cacheUnavailableVideo(@Nonnull String identifier, @Nonnull String unavailableReason);
+    void cacheUnavailableVideo(String identifier, String unavailableReason);
 
     /**
      * Called when a video format is unplayable for any reason, such as an invalid playback URL.
      * @param identifier The video ID.
      */
-    void removeVideoFormat(@Nonnull String identifier);
+    void removeVideoFormat(String identifier);
 
     /**
      * @param identifier The video ID.
      * @return The JSON object, as a string, or null if it isn't cached.
      */
-    @Nullable
-    YoutubeAudioTrack.FormatWithUrl getVideoFormat(@Nonnull String identifier);
+    YoutubeAudioTrack.FormatWithUrl getVideoFormat(String identifier);
 
     /**
      * @param identifier The video ID.
      * @return The reason why the video is unavailable, or null if it's not cached.
      */
-    @Nullable
-    String checkUnavailable(@Nonnull String identifier);
+    String checkUnavailable(String identifier);
 }
