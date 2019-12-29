34e401e596dc7a8878c61d7a4350d724e971dd64
sedmelluq <sedmelluq@gmail.com>
2019-12-29 23:04:19 +0200

    XM format merge.

---
+++ extensions/format-xm/src/main/java/com/sedmelluq/lavaplayer/extensions/format/xm/XmContainerProbe.java
@@ -0,0 +1,58 @@
+package com.sedmelluq.lavaplayer.extensions.format.xm;
+
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerDetectionResult;
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerHints;
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerProbe;
+import com.sedmelluq.discord.lavaplayer.tools.io.SeekableInputStream;
+import com.sedmelluq.discord.lavaplayer.track.AudioReference;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import ibxm.Module;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.sedmelluq.discord.lavaplayer.container.MediaContainerDetection.UNKNOWN_ARTIST;
+import static com.sedmelluq.discord.lavaplayer.container.MediaContainerDetectionResult.supportedFormat;
+
+public class XmContainerProbe implements MediaContainerProbe {
+  private static final Logger log = LoggerFactory.getLogger(XmContainerProbe.class);
+
+  @Override
+  public String getName() {
+    return "xm";
+  }
+
+  @Override
+  public boolean matchesHints(MediaContainerHints hints) {
+    return false;
+  }
+
+  @Override
+  public MediaContainerDetectionResult probe(AudioReference reference, SeekableInputStream inputStream) throws IOException {
+    Module module;
+    try {
+      module = new Module(inputStream);
+    } catch (IllegalArgumentException e) {
+      return null;
+    }
+
+    log.debug("Track {} is a module.", reference.identifier);
+
+    inputStream.seek(0);
+
+    return supportedFormat(this, null, new AudioTrackInfo(
+        module.songName,
+        UNKNOWN_ARTIST,
+        Long.MAX_VALUE,
+        reference.identifier,
+        true,
+        reference.identifier
+    ));
+  }
+
+  @Override
+  public AudioTrack createTrack(String parameters, AudioTrackInfo trackInfo, SeekableInputStream inputStream) {
+    return new XmAudioTrack(trackInfo, inputStream);
+  }
+}
