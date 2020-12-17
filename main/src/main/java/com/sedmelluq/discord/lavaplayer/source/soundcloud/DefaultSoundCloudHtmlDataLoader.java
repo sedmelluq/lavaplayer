bdae1c53aaf523cdfeef49b95dfa487dae28f9b8
sedmelluq <sedmelluq@gmail.com>
2020-12-17 22:39:23 +0200

    Make SoundCloud JSON extraction more change resistant and log html if no JSON found

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/DefaultSoundCloudHtmlDataLoader.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/DefaultSoundCloudHtmlDataLoader.java
@@ -1,6 +1,8 @@
 package com.sedmelluq.discord.lavaplayer.source.soundcloud;
 
 import com.sedmelluq.discord.lavaplayer.tools.DataFormatTools;
+import com.sedmelluq.discord.lavaplayer.tools.DataFormatTools.TextRange;
+import com.sedmelluq.discord.lavaplayer.tools.ExceptionTools;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.tools.JsonBrowser;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools;
@@ -11,10 +13,19 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity.SUSPICIOUS;
 
 public class DefaultSoundCloudHtmlDataLoader implements SoundCloudHtmlDataLoader {
+  private static final Logger log = LoggerFactory.getLogger(DefaultSoundCloudHtmlDataLoader.class);
+
+  private static final TextRange[] JSON_RANGES = {
+      new TextRange("catch(e){}})},", ");</script>"),
+      new TextRange("){}})},", ");</script>")
+  };
+
   @Override
   public JsonBrowser load(HttpInterface httpInterface, String url) throws IOException {
     try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(url))) {
@@ -25,17 +36,14 @@ public class DefaultSoundCloudHtmlDataLoader implements SoundCloudHtmlDataLoader
       HttpClientTools.assertSuccessWithContent(response, "video page response");
 
       String html = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-      String rootData = extractJsonFromHtml(html);
+      String rootData = DataFormatTools.extractBetween(html, JSON_RANGES);
 
       if (rootData == null) {
-        throw new FriendlyException("This url does not appear to be a playable track.", SUSPICIOUS, null);
+        throw new FriendlyException("This url does not appear to be a playable track.", SUSPICIOUS,
+            ExceptionTools.throwWithDebugInfo(log, null, "No track JSON found", "html", html));
       }
 
       return JsonBrowser.parse(rootData);
     }
-  }
-
-  protected String extractJsonFromHtml(String html) {
-    return DataFormatTools.extractBetween(html, "catch(e){}})},", ");</script>");
   }
 }
