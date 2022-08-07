39174be1f2779c1546ba1410bdefac84d09e0e3b
Gabriel Antunes <Gabriel>
2022-08-06 23:04:59 -0300

    fix: weird workaround to make hls work with some radios link

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/container/playlists/HlsStreamSegmentUrlProvider.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/container/playlists/HlsStreamSegmentUrlProvider.java
@@ -2,8 +2,10 @@ package com.sedmelluq.discord.lavaplayer.container.playlists;
 
 import com.sedmelluq.discord.lavaplayer.source.stream.M3uStreamSegmentUrlProvider;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
+
 import java.io.IOException;
 import java.util.List;
+
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.slf4j.Logger;
@@ -12,51 +14,52 @@ import org.slf4j.LoggerFactory;
 import static com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools.fetchResponseLines;
 
 public class HlsStreamSegmentUrlProvider extends M3uStreamSegmentUrlProvider {
-  private static final Logger log = LoggerFactory.getLogger(HlsStreamSegmentUrlProvider.class);
+    private static final Logger log = LoggerFactory.getLogger(HlsStreamSegmentUrlProvider.class);
 
-  private final String streamListUrl;
-  private volatile String segmentPlaylistUrl;
+    private final String streamListUrl;
+    private volatile String segmentPlaylistUrl;
 
-  public HlsStreamSegmentUrlProvider(String streamListUrl, String segmentPlaylistUrl) {
-    this.streamListUrl = streamListUrl;
-    this.segmentPlaylistUrl = segmentPlaylistUrl;
-  }
-
-  @Override
-  protected String getQualityFromM3uDirective(ExtendedM3uParser.Line directiveLine) {
-    return "default";
-  }
-
-  @Override
-  protected String fetchSegmentPlaylistUrl(HttpInterface httpInterface) throws IOException {
-    if (segmentPlaylistUrl != null) {
-      return segmentPlaylistUrl;
+    public HlsStreamSegmentUrlProvider(String streamListUrl, String segmentPlaylistUrl) {
+        this.streamListUrl = streamListUrl;
+        this.segmentPlaylistUrl = segmentPlaylistUrl;
     }
 
-    HttpUriRequest request = new HttpGet(streamListUrl);
-    List<ChannelStreamInfo> streams = loadChannelStreamsList(fetchResponseLines(httpInterface, request,
-        "HLS stream list"));
-
-    if (streams.isEmpty()) {
-      throw new IllegalStateException("No streams listed in HLS stream list.");
+    @Override
+    protected String getQualityFromM3uDirective(ExtendedM3uParser.Line directiveLine) {
+        return "default";
     }
 
-    ChannelStreamInfo stream = streams.get(0);
+    @Override
+    protected String fetchSegmentPlaylistUrl(HttpInterface httpInterface) throws IOException {
+        if (segmentPlaylistUrl != null) {
+            return segmentPlaylistUrl;
+        }
 
-    log.debug("Chose stream with url {}", stream.quality, stream.url);
-    segmentPlaylistUrl = stream.url;
-    return segmentPlaylistUrl;
-  }
+        HttpUriRequest request = new HttpGet(streamListUrl);
+        List<ChannelStreamInfo> streams = loadChannelStreamsList(fetchResponseLines(httpInterface, request,
+                "HLS stream list"));
 
-  @Override
-  protected HttpUriRequest createSegmentGetRequest(String url) {
-    return new HttpGet(url);
-  }
+        if (streams.isEmpty()) {
+            throw new IllegalStateException("No streams listed in HLS stream list.");
+        }
 
-  public static String findHlsEntryUrl(String[] lines) {
-    List<ChannelStreamInfo> streams = new HlsStreamSegmentUrlProvider(null, null)
-        .loadChannelStreamsList(lines);
+        ChannelStreamInfo stream = streams.get(0);
 
-    return streams.isEmpty() ? null : streams.get(0).url;
-  }
+        String uri = streamListUrl.substring(0, streamListUrl.lastIndexOf("/"));
+        segmentPlaylistUrl = uri + "/" + stream.url;
+        log.debug("Chose stream with url {}", stream.quality, uri);
+        return segmentPlaylistUrl;
+    }
+
+    @Override
+    protected HttpUriRequest createSegmentGetRequest(String url) {
+        return new HttpGet(url);
+    }
+
+    public static String findHlsEntryUrl(String[] lines) {
+        List<ChannelStreamInfo> streams = new HlsStreamSegmentUrlProvider(null, null)
+                .loadChannelStreamsList(lines);
+
+        return streams.isEmpty() ? null : streams.get(0).url;
+    }
 }
